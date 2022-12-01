ace06e87a4ba532170ef60292071d880955c40d4
HoangTran <trannhathoang231@gmail.com>
2022-12-01 11:08:32 +0700

    SỬ DỤNG FORM VÀ POST REQUEST VỚI NODE.JS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./route/web";
+const bodyParser  = require('body-parser');
 // import connection from "./configs/connectDB";
 
 require("dotenv").config(); //dong nay de doc file dotenv -> chay file .env chua PORT=3000
 
 const app = express();
 const port = process.env.PORT || 3000; //nếu port undefiend thì có ||3000 backup
+
+//Cấu hình Express gửi POST request:
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //setup view engine
 configViewEngine(app);
